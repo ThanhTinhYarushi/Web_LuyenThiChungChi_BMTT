9d8c96eb5c7466db2669eb7cb02a3c1a328ec5e1
thanhtinh <thanhtinh.yarushi@gmail.com>
2024-12-25 15:14:45 +0700

    exam exam-history

---
+++ pages/exam-history/exam-h.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   $('#exam-content').load(
+//     '/pages/exam/exam.html',
+//     function (response, status, xhr) {
+//       if (status == 'error') {
+//         $('#exam-content').html(
+//           '<h3>Không thể tải lịch thi. Vui lòng thử lại sau.</h3>'
+//         );
+//       }
+//     }
+//   );
+// });
