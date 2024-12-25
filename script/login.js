9a1b0c83a84a59fa6eb4a4fa277432512889e4e4
thanhtinh <thanhtinh.yarushi@gmail.com>
2024-12-25 13:46:33 +0700

    otp dashboard

--- script/login.js
+++ script/login.js
@@ -1,6 +1,8 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const loginBtnForm = document.getElementById('login-btn');
+const regBtnForm = document.getElementById('reg-btn');
 
 registerBtn.addEventListener('click', () => {
   container.classList.add('active');
@@ -8,4 +10,15 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
   container.classList.remove('active');
+});
+
+// open otp
+loginBtnForm.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = '../pages/otp/otp.html';
+});
+
+regBtnForm.addEventListener('click', (event) => {
+  event.preventDefault();
+  window.location.href = '../pages/otp/otp.html';
 });
